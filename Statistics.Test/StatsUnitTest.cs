680683f2b2e9d8824f555c96cf8a90812b33ba19
kavyashukla18 <69622475+kavyashukla18@users.noreply.github.com>
2020-09-03 15:21:58 +0530

    Update StatsUnitTest.cs

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -16,18 +16,18 @@ namespace Statistics.Test
             var computedStats = statsComputer.CalculateStatistics(
                 new List<double> { 1.5, 8.9, 3.2, 4.5 });
             float epsilon = 0.001F;
-            Assert.True(Math.Abs(statsComputer.average - 4.525) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.max - 8.9) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.min - 1.5) <= epsilon);
+            Assert.True(Math.Abs(computedStats.average - 4.525) <= epsilon);
+            Assert.True(Math.Abs(computedStats.max - 8.9) <= epsilon);
+            Assert.True(Math.Abs(computedStats.min - 1.5) <= epsilon);
         }
         public void ReportsNaNForEmptyInput()
         {
             var statsComputer = new StatsComputer();
             var computedStats = statsComputer.CalculateStatistics(
                 new List<double> {});
-            Assert.True(Double.IsNaN(statsComputer.average));
-            Assert.True(Double.IsNaN(statsComputer.max));
-            Assert.True(Double.IsNaN(statsComputer.min));
+            Assert.True(Double.IsNaN(computedStats.average));
+            Assert.True(Double.IsNaN(computedStats.max));
+            Assert.True(Double.IsNaN(computedStats.min));
         }
     }
 
