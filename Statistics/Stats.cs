16783b2f3903b568f5864a89ca14586854af8c7f
kavyashukla18 <69622475+kavyashukla18@users.noreply.github.com>
2020-09-03 15:03:29 +0530

    Create Stats.cs

---
+++ Statistics/Stats.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Statistics
+{
+    public class Stats
+    {
+        public float average { get; set; }
+        public float max { get; set; }
+        public float min { get; set; }
+    }
+}
